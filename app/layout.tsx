c022144d29eb801eb42b9dcbb8b3ba61ce43f3f6
Omer Halil Colak <omerhalilcolak@Omer-MacBook-Pro.local>
2026-02-09 15:23:59 +0300

    Use versioned favicon to bypass Chrome cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,11 @@ import "./globals.css";
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
+eexport const metadata: Metadata = {
   title: "LEVONDOR",
   description: "Sağlıklı ve uzun yaşam için akıllı teknolojiler.",
   icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32.png", type: "image/png", sizes: "32x32" },
-      { url: "/favicon-16.png", type: "image/png", sizes: "16x16" },
-    ],
-    apple: [{ url: "/apple-touch-icon.png" }],
+    icon: "/levondor.ico",
   },
 };
 
